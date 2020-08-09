3a9a55052eb6c532652c8357497185b35995bb5e
IrinaYu <irinayurchenko27@gmail.com>
2020-08-09 13:56:16 +0300

    first commit

---
+++ src/test/java/SampleSelenideTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.AssertionMode;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.testng.SoftAsserts;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+
+@Listeners ({ SoftAsserts.class})
+
+public class SampleSelenideTest {
+
+  @BeforeMethod
+  public void setUp(){
+    Configuration.browser = "chrome";
+  }
+
+
+  @Test
+  public void loginTest(){
+    open("https://jira.hillel.it/secure/Dashboard.jspa");
+    $(By.id("login-form-username")).setValue("webinar5");
+    $(By.id("login-form-password")).setValue("webinar5");
+    $("#login").click();
+    Configuration.assertionMode = AssertionMode.STRICT;
+    $(By.id("header-details-user-fullname")).shouldBe(Condition.visible);
+    //Configuration.assertionMode = Configuration.assertionMode.SOFT;
+
+  }
+}
